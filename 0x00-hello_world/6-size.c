702f942ed87d70ffbad26dffc065bf84a1d86be8
Elodie Riou <elodie.riou@hotmail.com>
2021-09-30 13:33:43 +0200

    Task 6. Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int intType;
+float floatType;
+double doubleType;
+char charType;
+return (0);
+}
