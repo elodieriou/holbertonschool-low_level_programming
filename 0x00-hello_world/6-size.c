3ea49cfbc44612bd992e1f2a18e2928b8fadcbfc
Elodie Riou <elodie.riou@hotmail.com>
2021-09-30 14:08:20 +0200

    Task 6. Size is not grandeur, and territory does not make a nation -> Modif faute de frappe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ printf("Size of a char: %lu byte(s)\n", sizeof(charType));
 printf("Size of an int: %lu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongintType));
-printf("Size of a float: %lu bytes(s)\n", sizeof(floatType));
+printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 return (0);
 }
