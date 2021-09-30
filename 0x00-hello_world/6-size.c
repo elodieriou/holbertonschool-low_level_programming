521dc6add4f1412caaee40eca4ae5506407a9519
Elodie Riou <elodie.riou@hotmail.com>
2021-09-30 14:01:55 +0200

    Task 6. Size is not grandeur, and territory does not make a nation -> Modif size of term

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,16 @@
  */
 int main(void)
 {
-int intType;
-float floatType;
-double doubleType;
 char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
 
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of a char: %lu byte(s)\n", sizeof(charType));
+printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %lu bytes(s)\n", sizeof(floatType));
 return (0);
 }
