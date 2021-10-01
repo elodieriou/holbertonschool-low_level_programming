15761d58371ea21cb8450dcc8814a417273a53d7
Elodie Riou <elodie.riou@hotmail.com>
2021-10-01 16:00:47 +0200

    Task 4 : Write a programm that prints the alphabet in lowercase, print all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		if ((n != 'q') && (n != 'e'))
+			putchar (n);
+	putchar ('\n');
+	return (0);
+}
