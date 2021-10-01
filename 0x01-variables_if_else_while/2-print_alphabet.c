a2bebe2f6449f27e06bf5d0e09fc35caa44d1374
Elodie Riou <elodie.riou@hotmail.com>
2021-10-01 14:51:26 +0200

    Task 2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game : Write a program that prints the alphabet in lowercase, followed by new line -> Add comment

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,19 @@ int main(void)
 	int n;
 
 	for (n = 'a'; n <= 'z'; n++)
+		/**
+		 * for (départ; condition; action)
+		 * départ : commence par 'a'
+		 * condition : tant que n n'est pas = z
+		 * action : ajoute +1 à chaque fois
+		 * la boucle s'exécutera 26 fois
+		 */
+
 		putchar (n);
+	/**
+	 *putchar : renvoie un entier
+	 */
+
 	putchar ('\n');
 	return (0);
 }
