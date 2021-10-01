697b64770771429d95d6de82d15f5f1fdf03d171
Elodie Riou <elodie.riou@hotmail.com>
2021-10-01 15:05:10 +0200

    Task 3. alphABET : Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	/**
+	 * print lowercase letters
+	 */
+	for (n = 'a'; n <= 'z'; n++)
+		putchar (n);
+	putchar ('\n');
+
+	/**
+	 * print uppercase letters
+	 */
+	for (n = 'A'; n <= 'Z'; n++)
+		putchar (n);
+	putchar ('\n');
+	return (0);
+}
