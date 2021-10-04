711040ec50743c5519562d8bd6071c82551d8cf0
Elodie Riou <elodie.riou@hotmail.com>
2021-10-04 16:44:58 +0200

    Task 11. Write a program that prints all possible different combinations of three digits.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+	int n3;
+
+	for (n1 = '0'; n1 <= '9'; n1++)
+	{
+		for (n2 = '0'; n2 <= '9'; n2++)
+		{
+			for (n3 = '0'; n3 <= '9'; n3++)
+			{
+				if (n1 < n2 && n2 < n3)
+				{
+					putchar (n1);
+					putchar (n2);
+					putchar (n3);
+					if (n1 != '7')
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
