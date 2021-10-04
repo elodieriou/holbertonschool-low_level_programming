e626e379f8e376ef486691ac4cfee5968722567c
Elodie Riou <elodie.riou@hotmail.com>
2021-10-04 15:34:26 +0200

    Task 10. Write a program that prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+
+	for (n1 = '0'; n1 >= '9'; n1++)
+	{
+		for (n2 = '0'; n2 <= '9'; n2++)
+		{
+			if (n1 < n2)
+			{
+				putchar (n1);
+				putchar (n2);
+			}
+			if (n1 != '8')
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
