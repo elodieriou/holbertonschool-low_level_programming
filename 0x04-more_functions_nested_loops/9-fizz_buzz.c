e1a017cd554e3024a9235b10c781235bf3d03ffc
Elodie Riou <elodie.riou@hotmail.com>
2021-10-07 17:27:35 +0200

    Task 9: Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ * if multiples of three print Fizz
+ * if multiples of five print Buzz
+ * if mutliples of three and five print FizzBuzz
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf ("FizzBuzz");
+		}
+		else if (n % 3  == 0)
+		{
+			printf ("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf ("Buzz");
+		}
+		else
+		{
+			printf ("%d", n);
+		}
+		printf (" ");
+	}
+	printf ("\n");
+	return (0);
+}
