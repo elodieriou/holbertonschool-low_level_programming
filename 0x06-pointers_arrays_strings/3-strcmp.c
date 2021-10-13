b94f8bb9259e94cdb5550e6d59ac5c4eb8bbfa4a
Elodie Riou <elodie.riou@hotmail.com>
2021-10-13 13:52:46 +0200

    Add file 3-strcmp -> function thatcompares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ *
+ * @s1: string compare to string s2
+ * @s2: string compare to sting s1
+ *
+ * Return: an integer less than equal, greater or lower
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
