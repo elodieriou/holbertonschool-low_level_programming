0efa7863c604fb8cd2d875629c00350b7872e001
Elodie Riou <elodie.riou@hotmail.com>
2021-10-13 13:37:33 +0200

    Add file 2-strncpy -> function that copies a string with n bytes of src copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include <sting.h>
+#include "main.h"
+
+/**
+ * *_strncpy - function that copies a string
+ *
+ * @dest: receive string copy of src
+ * @src: sting copy in dest
+ * @n: copy n bytes of src
+ *
+ * Return: a pointer of variable dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
