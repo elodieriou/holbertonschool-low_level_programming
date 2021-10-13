0372bf8e10c7c89e2ae64d7b1997871a7db4ce39
Elodie Riou <elodie.riou@hotmail.com>
2021-10-13 13:25:44 +0200

    Fix betty warnings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,13 @@
 #include "main.h"
 
 /**
- * *_strncat - function concatenates two sting that il will use at most n bytes from src
- * and src does not need to be null-terminated if it contains n or more bytes
+ * *_strncat - function concatenates two sting that il will use at most n bytes
+ * from src and src does not need to be null-terminated if it contains  n
+ * or more bytes
  *
  * @dest: string that receives a copy of variable src string
  * @src: string copy in the variable dest string
+ * @n: bytes from src
  *
  * Return: a pointer to the resulting string dest
  */
