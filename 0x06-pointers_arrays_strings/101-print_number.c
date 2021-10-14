fc1830982e2b182af4c12d8c51b5c97b60ba4dbe
Elodie Riou <elodie.riou@hotmail.com>
2021-10-14 15:44:08 +0200

    Fix betty warnings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * print_number - print an integer
- *
- * @n: variable integer
- */
+  * print_number - Prints an integer
+  *
+  * @n: a number
+  */
 void print_number(int n)
 {
-	int i;
+	unsigned int i;
 
 	if (n < 0)
 	{
-		_putchar ('-');
-		n = -n;
+		_putchar('-');
+		n *= -1;
 	}
 
 	i = n;
 
-	if (i / 10 > 0)
-	{
+	if (i / 10)
 		print_number(i / 10);
-	}
-	_putchar (i % 10 + '0');
+
+	_putchar(i % 10 + '0');
 }
