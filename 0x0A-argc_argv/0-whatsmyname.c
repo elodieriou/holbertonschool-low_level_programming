a5b999f8f1c53af30bad29559d9026124150e870
Elodie Riou <elodie.riou@hotmail.com>
2021-10-22 09:52:40 +0200

    Add file 0-whatsmyname.c -> a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the program's name
+ *
+ * @argc: the number of command line arguments
+ * @argv: an array of size argc
+ *
+ * Return: Always success
+ */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
