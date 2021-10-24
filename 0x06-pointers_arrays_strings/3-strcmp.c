cb70b928ffd09be50eb75854021ae78e9fc56622
Elodie Riou <elodie.riou@hotmail.com>
2021-10-24 17:54:26 +0200

    Fix code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,5 +11,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(s1, s2));
+	while(*s1 == *s2)
+	{
+		if(*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
