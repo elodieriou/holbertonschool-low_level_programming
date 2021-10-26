2b860527d7e4da5b6177d0de448d1f121d4fc3bf
Elodie Riou <elodie.riou@hotmail.com>
2021-10-26 10:44:01 +0200

    Fix code if malloc is null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,14 @@ char *create_array(unsigned int size, char c)
 	{
 		return ('\0');
 	}
+
 	str = malloc(size * sizeof(char));
+
+	if (str == 0)
+	{
+		return ('\0');
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
