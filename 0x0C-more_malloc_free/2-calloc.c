2136f706f4afb164146210439a4dd3264a62101b
Elodie Riou <elodie.riou@hotmail.com>
2021-10-28 14:43:32 +0200

    Fix code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,18 +12,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ar;
-	unsigned int mul = 0, i = 0;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	mul = nmemb * size;
-
-	ar = malloc(mul);
+	ar = malloc(nmemb * size);
 	if (ar == 0)
 		return (NULL);
 
-	for (i = 0; i < mul; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		*ar = 0;
 	return (ar);
 }
