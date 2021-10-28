6e1bffa50da0b6e6288cca6fe1da70ad6e1d82f7
Elodie Riou <elodie.riou@hotmail.com>
2021-10-28 15:07:24 +0200

    Fix code with function memset

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,24 @@
 #include "main.h"
 
 /**
+ * _memset - function that fills memory with a constant byte
+ *
+ * @s: pointer to a character
+ * @b: a character
+ * @n: n bytes of the memory area pointed by s
+ *
+ * Return: value of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
  * _calloc - function that allocates memory for an array, using malloc
  *
  * @nmemb: n elements
@@ -12,16 +30,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ar;
-	unsigned int i;
+	unsigned int mul = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ar = malloc(nmemb * size);
+	mul = nmemb * size;
+
+	ar = malloc(mul);
 	if (ar == 0)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
-		*ar = 0;
+	_memset(ar, 0, mul);
 	return (ar);
 }
