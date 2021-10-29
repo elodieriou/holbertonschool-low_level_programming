e45da90975faa12df73c87d1467bbebc590186eb
Elodie Riou <elodie.riou@hotmail.com>
2021-10-29 10:09:19 +0200

    Add file 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ *
+ * @s: a string
+ *
+ * Return: size of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while(s[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * _memcpy - fucntion that copies memory area
+ *
+ * @dest: copy of memory area src
+ * @src: memory area copy on dest
+ * @n: n bytes copies
+ *
+ * Return: value of dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
+/**
+ * _realloc - function that reallocates a memory block using malloc and free
+ *
+ * @prt: is a pointer to the memory previously allocated
+ * @old_size: is the size in bytes of the allocated space for ptr
+ * @new_size: is the new size in bytes of the new memory block
+ *
+ * Return: Nothing
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *nptr;
+
+	ptr = malloc(old_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == 0)
+		ptr = malloc(new_size);
+
+	if (new_size == 0 && ptr != NULL)
+		return (NULL);
+
+	nptr = malloc(new_size);
+	_memcpy(nptr, ptr, _strlen(ptr));
+
+	free(ptr);
+	return (nptr);
+}
