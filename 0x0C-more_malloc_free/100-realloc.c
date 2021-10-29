ab6181b92cfb131d86cec79a73675fbc70ab7bb4
Elodie Riou <elodie.riou@hotmail.com>
2021-10-29 10:55:14 +0200

    Fix code

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,17 +34,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *nptr;
 
 	if (ptr == 0)
-        {
-                free(ptr);
-                ptr = malloc(new_size);
-                return (ptr);
-        }
+	{
+		free(ptr);
+		ptr = malloc(new_size);
+		return (ptr);
+	}
 
 	if (new_size == old_size)
 		return (ptr);
 
 	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
 	nptr = malloc(new_size);
 	if (nptr == 0)
