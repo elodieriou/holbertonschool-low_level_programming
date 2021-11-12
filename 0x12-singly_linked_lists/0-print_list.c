9994a74963717c62957da11c0f9b21f7ab206159
Elodie Riou <elodie.riou@hotmail.com>
2021-11-12 11:22:07 +0100

    First commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a struct list
+ * @h: a pointer to a struct
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
