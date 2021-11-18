f2c9be2ddbd02e26c770fa0deaef0e5c7b7c3383
Elodie Riou <elodie.riou@hotmail.com>
2021-11-18 14:46:08 +0100

    fix code with if index >= 64 return -1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,17 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int len = 0, num = 0;
-
-	num = n;
-	while (n)
-	{
-		n >>= 1;
-		len++;
-	}
-
-	if (index >= len)
+	if (index >= 64)
 		return (-1);
 	else
-		return (num >> index & 1);
+		return (n >> index & 1);
 }
