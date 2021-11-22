261262dbcebeae5e432d576a3befbee285c47cdc
Elodie Riou <elodie.riou@hotmail.com>
2021-11-22 14:54:37 +0100

    fix code with strlen(text_content)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the lenght of a string
+ *
+ * @s: string
+ *
+ * Return: size of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
+
+/**
  * create_file - function that creates a file
  * @filename: a pointer to the filename
  * @text_content: is a NULL terminated string to write to the file
@@ -8,22 +26,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len;
+	int o;
+	char *buffer = text_content;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, buffer, len);
-
-	if (o == -1 || w == -1)
+	if (o == -1)
 		return (-1);
+
+	if (text_content)
+		write(o, buffer, _strlen(text_content));
 
 	close(o);
 	return (1);
