fb6f4dc7df51f4169047f4bdf92436d8f153d771
Elodie Riou <elodie.riou@hotmail.com>
2021-11-22 14:46:05 +0100

    fix code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,12 +21,12 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, buffer, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
-	return(1);
+	return (1);
 }
