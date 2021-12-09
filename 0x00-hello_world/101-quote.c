a20cb2afd4a311366f2f5f73213ce2a2d0e4765c
Elodie Riou <elodie.riou@hotmail.com>
2021-12-09 21:49:06 +0100

    add STDERR_FILENO

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-write(2, "and that piece of art if useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(STDERR_FILENO,
+	      "and that piece of art if useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+	return (1);
 }
