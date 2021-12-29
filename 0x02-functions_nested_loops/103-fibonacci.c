90fc9e12b01d6cfa1cd376dfd2d2973c008ab252
Elodie Riou <elodie.riou@hotmail.com>
2021-12-29 11:17:48 +0100

    add function that prints the Fibonacci sequence whose values do not exceed 4,000,000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function that print the Fibonacci sequence whose values
+ * do not exceed 4,000,000
+ * Return: 0 always success
+ */
+int main(void)
+{
+	long int i, num1 = 0, num2 = 1, sum = 0, nextNum;
+
+	for (i = 0; i < 50; i++)
+	{
+		nextNum = num1 + num2;
+		num1 = num2;
+		num2 = nextNum;
+
+		if (nextNum % 2 == 0 && nextNum < 4000000)
+			sum += nextNum;
+	}
+	printf("%ld", sum);
+	return (0);
+}
