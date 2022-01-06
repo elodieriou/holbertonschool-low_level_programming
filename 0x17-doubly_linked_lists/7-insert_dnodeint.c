9b4e11299e34ec110ce5e20f52a0ec1ccf2b44f0
Elodie Riou <elodie.riou@hotmail.com>
2022-01-06 13:36:53 +0100

    add function that inserts a new node at a given position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that returns the number of node
+ * @h: a pointer at the beginning of a (doubly) linked list
+ * Return: the number of node
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
+
+/**
+ * insert_dnodeint_at_index - function that inserts a new node at
+ * a given position
+ * @h: a double pointer at the beginning of a doubly linked list
+ * @idx: the position where the new node should be added
+ * @n: the data of a node
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	size_t len_node = 0;
+	dlistint_t *new_node, *tmp = *h;
+	unsigned int i;
+
+	len_node = dlistint_len(*h);
+	if (idx > len_node)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL || h == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (*h == NULL)
+	{
+		*h = new_node;
+		return (new_node);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		*h = new_node;
+		return (new_node);
+	}
+
+	for (i = 0; i < idx - 1; i++)
+		tmp = tmp->next;
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+	return (new_node);
+}
