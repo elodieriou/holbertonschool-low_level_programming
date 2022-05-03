a6701c0c476269f07ab5dcf099a35c964bc8b39d
Elodie Riou <elodie.riou@hotmail.com>
2022-05-03 13:13:50 +0200

    fix code

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -11,10 +11,14 @@ void print_array(int *array, size_t left, size_t right)
 {
 	size_t i;
 
-	printf("Searching in array:");
-	for (i = left; i < right; i++)
-		printf(" %d, ", array[i]);
-	printf(" %d\n", array[i]);
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i != right)
+			printf(", ");
+	}
+	printf("\n");
 }
 
 /**
