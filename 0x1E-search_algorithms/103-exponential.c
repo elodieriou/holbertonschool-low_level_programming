1869728f8f748512622dcbc67f2f5f5555bbc5c5
Elodie Riou <elodie.riou@hotmail.com>
2022-05-03 12:01:02 +0200

    fix betty warnings

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -9,19 +9,20 @@
  */
 void print_array(int *array, size_t left, size_t right)
 {
-    size_t i;
+	size_t i;
 
-    printf("Searching in array:");
-    for (i = left; i < right; i++)
-        printf(" %d, ", array[i]);
-    printf(" %d\n", array[i]);
+	printf("Searching in array:");
+	for (i = left; i < right; i++)
+		printf(" %d, ", array[i]);
+	printf(" %d\n", array[i]);
 }
 
 /**
- * binear_search - function that searches for a value in an sorted of integers
+ * binary_search_linear - function that searches for a value in an sorted array
  * using Binear Search Algorithm
  * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
+ * @start: the beginning of the array
+ * @end: the end of the array
  * @value: the value to search at an index
  * Return: the first index where value is located
  *         -1 if value is not present in array
@@ -29,25 +30,25 @@ void print_array(int *array, size_t left, size_t right)
  */
 int binary_search_linear(int *array, size_t start, size_t end, int value)
 {
-    size_t mid;
+	size_t mid;
 
-    printf("Value found between indexes [%ld] and [%ld]\n", start, end);
-    if (array == NULL)
-        return (-1);
+	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+	if (array == NULL)
+		return (-1);
 
-    while (end >= start)
-    {
-        print_array(array, start, end);
-        mid = start + (end - start) / 2;
+	while (end >= start)
+	{
+		print_array(array, start, end);
+		mid = start + (end - start) / 2;
 
-        if (value == array[mid])
-            return (mid);
-        if (value > array[mid])
-            start = mid + 1;
-        else
-            end = mid - 1;
-    }
-    return (-1);
+		if (value == array[mid])
+			return (mid);
+		if (value > array[mid])
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	return (-1);
 }
 
 /**
@@ -58,13 +59,13 @@ int binary_search_linear(int *array, size_t start, size_t end, int value)
  */
 int min_value(int a, int b)
 {
-    if (a > b)
-        return (b);
-    return (a);
+	if (a > b)
+		return (b);
+	return (a);
 }
 
 /**
- * exponential_search - function that searches for a value in an sorted of integers
+ * exponential_search - function that searches for a value in an sorted array
  * using Exponential Search Algorithm
  * @array: a pointer to the first element of the array to search in
  * @size: the number of elements in array
@@ -75,19 +76,20 @@ int min_value(int a, int b)
  */
 int exponential_search(int *array, size_t size, int value)
 {
-    size_t index;
+	size_t index;
 
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    if (array[0] != value)
-    {
-        index = 1;
-        while (index < size && array[index] <= value)
-        {
-            printf("Value checked array[%ld] = [%d]\n", index, array[index]);
-            index *= 2;
-        }
-    }
-    return (binary_search_linear(array, index / 2, min_value(index, size - 1), value));
+	if (array[0] != value)
+	{
+		index = 1;
+		while (index < size && array[index] <= value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+			index *= 2;
+		}
+	}
+	return (binary_search_linear(array, index / 2,
+				     min_value(index, size - 1), value));
 }
